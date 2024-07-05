0ea5b129bbabb8e8dd28a5e81d9b0ede99687ff3
koiMeow <ja930036@gmail.com>
2024-07-05 18:47:49 +0800

    log in and out function completed.

--- src/pages/Home/components/Main.js
+++ src/pages/Home/components/Main.js
@@ -3,7 +3,7 @@ import { v4 } from "uuid";
 
 import Post from "./Post";
 
-const Main = ({ msgList, add, isLoggedIn }) => {
+const Main = ({ msgList, add, register, login, logout, isLoggedIn, user }) => {
   const tabs = ["匿名留言", "開發中..."];
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
@@ -13,6 +13,37 @@ const Main = ({ msgList, add, isLoggedIn }) => {
   });
 
   // 註冊及登入帳號
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSignUp = async () => {
+    try {
+      await register(email, password);
+      window.alert("註冊成功");
+    } catch (error) {
+      window.alert("註冊失敗");
+      console.error("註冊失敗: ", error.message);
+    }
+  };
+
+  const handleSignIn = async () => {
+    try {
+      await login(email, password);
+      window.alert("登入成功");
+    } catch (error) {
+      window.alert("登入失敗");
+      console.error("登入失敗: ", error.message);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logout();
+      window.alert("登出成功");
+    } catch (error) {
+      window.alert("登出失敗");
+      console.error("登出失敗: ", error.message);
+    }
+  }
 
   // 輸入訊息
   const handleChange = (e) => {
@@ -64,6 +95,7 @@ const Main = ({ msgList, add, isLoggedIn }) => {
       <div className="bottom">
         {isLoggedIn ? (
           <>
+            <div className="useremail">{user.email}</div>
             <input
               type="text"
               name="message"
@@ -71,18 +103,31 @@ const Main = ({ msgList, add, isLoggedIn }) => {
               onChange={handleChange}
               onKeyDown={handleKeyDown}
             />
-            <button onClick={sendMessage}>送出</button>
+            <button className="function-button" onClick={sendMessage}>送出</button>
+            <button className="function-button" onClick={handleSignOut}>登出</button>
           </>
         ) : (
           <>
             <div></div>
             <div className="logginarea">
-              <>帳號</>
-              <input />
+              <>電子郵件</>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <>密碼</>
-              <input />
-              <button className="function-button">登入</button>
-              <button className="function-button">註冊</button>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="function-button" onClick={handleSignIn}>
+                登入
+              </button>
+              <button className="function-button" onClick={handleSignUp}>
+                註冊
+              </button>
             </div>
           </>
         )}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,53 @@
-  import Top from "./components/Top";
-  import Main from "./components/Main";
+import Top from "./components/Top";
+import Main from "./components/Main";
 
-  import "./css/index.css";
-  import { useEffect, useState } from "react";
-  import { db, collection, addDoc, onSnapshot } from "../../firebase";
+import "./css/index.css";
+import { useEffect, useState } from "react";
+import { db, collection, addDoc, onSnapshot } from "../../firebase";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-  const Home = () => {
-    const [data, setData] = useState([]);
+const Home = () => {
+  // 聊天訊息資料
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-      const unsubscribe = onSnapshot(collection(db, "messages"), (snapshot) => {
-        const messageData = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "messages"), (snapshot) => {
+      const messageData = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
 
-        messageData.sort((b, a) => a.time - b.time);
-        setData(messageData);
-      });
+      messageData.sort((b, a) => a.time - b.time);
+      setData(messageData);
+    });
 
-      return () => unsubscribe();
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-    const addMessage = (formData) => addDoc(collection(db, "messages"), formData);
-    
-    return (
-      <div>
-        <Top />
-        <Main msgList={data} add={addMessage} isLoggedIn={false}/>
-      </div>
-    );
-  };
+  const addMessage = (formData) => addDoc(collection(db, "messages"), formData);
 
-  export default Home;
+  // 使用者帳號密碼資料
+  const auth = getAuth();
+  const [user] = useAuthState(auth);
+  const createAccount = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const loginAccount = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  }
+
+  const logoutAccount = () => {
+    signOut(auth);
+  }
+
+  return (
+    <div>
+      <Top />
+      <Main msgList={data} add={addMessage} register={createAccount} login={loginAccount} logout={logoutAccount} isLoggedIn={!!user} user={user} />
+    </div>
+  );
+};
+
+export default Home;
